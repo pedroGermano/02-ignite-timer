5244f1bb0bb14a11cd1e575f621481ba01769ae7
Pedro Germano <pedrogermano232@gmail.com>
2023-03-30 17:19:38 -0300

    edit global styles

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,12 +16,10 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border: 0;
+  margin: 8px;
+  border-radius: 2px;
 
-  background: ${(props) => props.theme.primary};
-
-  /* ${(props) => {
-    return css`
-      background: ${buttonVariants[props.variant]};
-    `;
-  }} */
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 `;
