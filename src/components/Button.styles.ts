d853011b8b31618a8fafeb4c1393da23f20ef281
Pedro Germano <pedrogermano232@gmail.com>
2023-03-30 16:57:33 -0300

    create styles global

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
@@ -17,7 +17,11 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${(props) => {
-    return `background: ${buttonVariants[props.variant]}`;
-  }}
+  background: ${(props) => props.theme.primary};
+
+  /* ${(props) => {
+    return css`
+      background: ${buttonVariants[props.variant]};
+    `;
+  }} */
 `;
