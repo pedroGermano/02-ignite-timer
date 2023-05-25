0ba84dc8fb8718a385cac929de1705190da819c3
Pedro Germano <pedrogermano232@gmail.com>
2023-05-25 17:23:24 -0300

    title change

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -48,13 +48,19 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         );
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
@@ -81,6 +87,12 @@ export function Home() {
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
 
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
+
   const task = watch("task");
   const isSubmitDisabled = !task;
 
@@ -99,7 +111,6 @@ export function Home() {
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
             <option value="Projeto 2" />
-            <option value="Projeto 3" />
             <option value="Projeto 3" />
           </datalist>
 
